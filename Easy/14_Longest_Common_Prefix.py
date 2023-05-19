9c2ccb210df10b9ba1f693b8b08ebf8e009441db
skyshineb <ksky7000@gmail.com>
2023-05-19 19:53:51 +0700

    batch 5

--- Easy/14_Longest_Common_Prefix.py
+++ Easy/14_Longest_Common_Prefix.py
@@ -2,27 +2,43 @@ from typing import List
 
  # My mind is dazed rn, this solution is not working
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix_workingButBad(self, strs: List[str]) -> str:
         if len(strs) == 1:
             return strs[0]
         res = list(ord(strs[0][x]) for x in range(0, len(strs[0])))
-        length = 0
+        maxInd = 999
         for i in range(1, len(strs)):
             istr = strs[i]
+            p = 0
             for j in range(0, min(len(istr), len(res))):
                 if res[j] ^ ord(istr[j]) == 0:
-                    if length < j + 1:
-                        length = j
+                    p += 1
                 else:
                     break
-            res = list(ord(istr[x]) for x in range(0, len(istr)))
+            if maxInd > p:
+                maxInd = p
+                res = list(ord(istr[x]) for x in range(0, len(istr)))
 
-        if length > 0:
-            return strs[0][0:length - 1]
+        if maxInd > 0:
+            return strs[0][0:maxInd]
         else:
             return ""
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+        out = strs[0]
+        for s in strs[1:]:
+            maxI = 0
+            for i,c in enumerate(s):
+                if i < len(out) and out[i] == c:
+                    maxI += 1
+                else:
+                    break
+            out = out[0: maxI]
+        return out
 
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.longestCommonPrefix(["ab", "a"]))
+    print(sol.longestCommonPrefix(["cir","car"]))
